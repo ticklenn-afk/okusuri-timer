e9d1ff189e1011ccea6a0ae5f6390bc705534167
Hiro <ticklenn@gmail.com>
2026-05-02 00:20:54 +0900

    debug: エラー表示を追加

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+try {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+} catch (e) {
+  document.getElementById('root').innerHTML =
+    '<div style="padding:20px;color:red;font-size:14px;word-break:break-all">' +
+    'エラー: ' + e.message + '<br/><br/>' + e.stack + '</div>'
+}
