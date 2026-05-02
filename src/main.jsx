4648299779bfc1b095cb715d292940f50171b67b
Hiro <ticklenn@gmail.com>
2026-05-02 22:46:43 +0900

    fix: デバッグ用赤いメッセージを削除

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-document.getElementById('js-debug')?.remove()
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
